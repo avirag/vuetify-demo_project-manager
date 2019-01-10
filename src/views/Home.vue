ef35252ed2c195db923a8f75da405e1afc93b432
Anita Virag <anitavirag.dev@gmail.com>
2019-01-10 12:38:13 +0100

    button and icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,29 @@
     <p class="headline">This is a headline</p>
     <p class="subheading font weight bold">This is a subheading (with font weight bold)</p>
     <p class="caption">This is a caption</p>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed dark color="pink">click me</v-btn>
+    <v-btn flat dark color="pink">click me</v-btn>
+
+    <v-btn depressed color="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small color="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large color="pink white--text">
+      <span>email me</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
   </div>
 </template>
 
