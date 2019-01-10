1c53223780bf5eb3c2549c467bf46608544e80a8
Anita Virag <anitavirag.dev@gmail.com>
2019-01-10 12:22:59 +0100

    text and colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum corrupti delectus facilis dicta dolores ex aliquam odio accusantium, numquam vel enim perferendis voluptas libero modi earum molestiae, nam beatae inventore?</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit incidunt commodi blanditiis ullam repudiandae cupiditate, dicta esse molestiae soluta dolorem ratione, exercitationem distinctio quo pariatur nostrum deserunt modi iure magni.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font weight bold">This is a subheading (with font weight bold)</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
